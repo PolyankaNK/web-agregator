316942abaeb548af6f0238abcacb35fb851db25f
Nikita Polyansky <nik.polyanka@gmail.com>
2026-03-01 19:29:20 +0200

    Add service detail view and update service list with search functionality

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("services/", views.service_list, name="service_list"),
+    path('services/<int:pk>/', views.service_detail, name='service_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from core.models import ServiceCenter
+from .models import ServiceCenter
 
 def home(request):
     return render(request, 'core/home.html')
 
 def service_list(request):
+    query = request.GET.get('q')
     services = ServiceCenter.objects.all()
+    if query:
+        services = services.filter(name__icontains=query)
     context = {
         "services": services
     }
     return render(request, "core/service_list.html", context)
+
+def service_detail(request, pk):
+    service = get_object_or_404(ServiceCenter, pk=pk)
+    return render(request, 'core/service_detail.html', {'service': service})
